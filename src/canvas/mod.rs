dfc00cdb1e2a563f4ba80e7c0bcb95f9a86d5a52
Patrick Muldoon <doon@labratsoftware.com>
2018-12-25 14:44:25 -0800

    start of canvas and some organization

---
+++ src/canvas/mod.rs
@@ -0,0 +1,51 @@
+use crate::base;
+
+struct Canvas {
+    width: usize,
+    height: usize,
+    pixels: Vec<Vec<base::Tuple>>,
+}
+
+impl Canvas {
+    fn new(width: usize, height: usize) -> Canvas {
+        let pixels = vec![vec![base::Tuple::color(0.0, 0.0, 0.0); width]; height];
+        Canvas {
+            width: width,
+            height: height,
+            pixels: pixels,
+        }
+    }
+
+    fn write_pixel(&mut self, x: usize, y: usize, color: base::Tuple) {
+        self.pixels[y][x] = color;
+    }
+
+    fn pixel_at(&self, x: usize, y: usize) -> &base::Tuple {
+        &self.pixels[y][x]
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn creating_a_canvas() {
+        let c = Canvas::new(10, 20);
+        assert_eq!(c.width, 10);
+        assert_eq!(c.height, 20);
+        for (_y, row) in c.pixels.iter().enumerate() {
+            for (_x, pixel) in row.iter().enumerate() {
+                assert_eq!(pixel, &base::Tuple::color(0.0, 0.0, 0.0));
+            }
+        }
+    }
+
+    #[test]
+    fn writing_pixel_to_canvas() {
+        let mut c = Canvas::new(10, 20);
+        let red = base::Tuple::color(1.0, 0.0, 0.0);
+        c.write_pixel(2, 3, red);
+        assert_eq!(c.pixel_at(2, 3), &red);
+    }
+}
